f1e97c560e410a24a227d0fdccc6083140fa6553
Sérgio Junior <sergioamjr91@gmail.com>
2018-02-07 18:40:39 -0200

    Consultando valor atual do bitcoin

--- src/main.js
+++ src/main.js
@@ -1,1 +1,67 @@
-console.log('main@s');
+const ccxt = require('ccxt')
+
+const mercado = new ccxt.mercado()
+mercado
+    .loadMarkets()
+
+// let count = 0;
+let now = undefined
+let minPercent = 1
+let maxPercent = 2
+
+const setNow = (data, update = false) => {
+    const { last: value } = data
+    if(!now || update) {
+        console.log('Atualiza valor base', update, value)
+        now = value
+    }
+    return data
+}
+
+const loop = setInterval(() => {
+    // count++
+    mercado.fetchTicker('BTC/BRL')
+        .then(setNow)
+        .then((data) => {
+            const { last } = data
+            if(last > now) {
+                console.log('Vende', last, now)
+                setNow(data, true)
+            } else if (last < now) {
+                console.log('Compra', last, now)
+                setNow(data, true)
+            }
+        })
+    // if(count >= 5) {
+    //     clearInterval(loop)
+    // }
+}, 5000)
+
+function init() {
+    
+}
+
+// cex
+//     .loadMarkets()
+//     .then(data => console.log(data))
+//     .then(() =>
+//         cex.fetchTicker('ETH/USD')
+//             .then(data => console.log('ticket', data))
+//     )
+
+
+// (async () => {
+//     await cex.loadMarkets()
+//     const ticker = await cex.fetchTicker('BTC/USD')
+//     console.log(ticker)
+// })()
+
+// let count = 0;
+// const loop = setInterval(() => {
+//     count++
+//     cex.fetchMarkets().then((data) => console.log(data[0]))
+//     if(count >= 5) {
+//         clearInterval(loop)
+//     }
+// }, 2000)
+
