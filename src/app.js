b6b3ad80c7d73e715fe96544abb8594cdb673092
Sérgio Junior <sergioamjr91@gmail.com>
2018-02-08 18:53:27 -0200

    Configurado webpack dev e prod

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './p'
+
+const Hello = () => <p>Hello Reacts</p>
+
+export default Hello
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,13 @@
 const path = require('path');
-const indexHtml = path.join(__dirname, "src/index.html");
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: "source-map",
     target: "web",
     entry: [
         "./src/index.js",
-        indexHtml,
     ],
     resolve: {
         extensions: ['.js', '.jsx', '.css', '.scss'],
@@ -29,39 +28,16 @@ module.exports = {
         rules: [
             {
                 test: /\.scss$/,
-                use: [{
-                    loader: "style-loader"
-                  }, {
-                    loader: "css-loader" 
-                  }, {
-                    loader: "sass-loader"
-                  }]
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                }),
+                exclude: /node_modules/,
             },
             {
                 test: /\.css$/,
                   use: ['style-loader', 'css-loader']
               },
-            {
-                test: indexHtml,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: "[name].[ext]",
-                        },
-                    },
-                    {
-                        loader: "extract-loader",
-                    },
-                    {
-                        loader: "html-loader",
-                        options: {
-                            attrs: ["img:src", "link:href"],
-                            interpolate: true,
-                        },
-                    },
-                ],
-            },
             {
                 test: /\.(js|jsx)$/,
                 loader: 'babel-loader',
@@ -78,6 +54,9 @@ module.exports = {
         ]
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Wall-B',
+        }),
         //if you want to pass in options, you can do so:
         new ExtractTextPlugin({
             filename: 'style.css',
